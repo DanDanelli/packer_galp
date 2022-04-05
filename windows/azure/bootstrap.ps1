625a8466456955d2c7c8adb9289e8d443d8e63ba
Daniel <danieldanelli@gmail.com>
2022-04-05 09:14:41 +0100

    final version

--- windows/azure/bootstrap.ps1
+++ windows/azure/bootstrap.ps1
@@ -1,9 +1,28 @@
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-Set-Service -Name sshd -StartupType 'Automatic'
-Start-Service sshd
-New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
-Get-Service -Name ssh-agent | Set-Service -StartupType 'Automatic'
-Start-Service ssh-agent
-Stop-Service sshd
-Start-Service sshd
-Add-Content $Env:ProgramData\ssh\sshd_config "TrustedUserCAKeys __PROGRAMDATA__/ssh/trusted-user-ca-keys.pem"
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -ErrorAction Ignore
+$ErrorActionPreference = "stop"
+
+# Set administrator password
+net user packer SuperS3cr3t!!!!
+wmic useraccount where "name='packer'" set PasswordExpires=FALSE
+
+# turn off PowerShell execution policy restrictions
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine
+
+# configure WinRM
+winrm quickconfig -q
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="2048"}'
+winrm set winrm/config/winrs '@{MaxConcurrentUsers="100"}'
+winrm set winrm/config/winrs '@{MaxProcessesPerShell="0"}'
+winrm set winrm/config/winrs '@{MaxShellsPerUser="0"}'
+winrm set winrm/config '@{MaxTimeoutms="7200000"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/service/auth '@{CredSSP="true"}'
+winrm set winrm/config/client '@{TrustedHosts="*"}'
+
+
+# open port 5986 in the internal Windows firewall to allow WinRM communication
+netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
+
+net stop winrm
+net start WinRM
